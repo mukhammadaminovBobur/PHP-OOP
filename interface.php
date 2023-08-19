f638cac33dd45d071b6d5f338ca39969f33e5a4f
Muhammadbobur Muhammaddaminov <virtualcoder20@gmail.com>
2023-08-19 23:05:01 +0500

    Interface

---
+++ interface.php
@@ -0,0 +1,31 @@
+<?php
+//Interface / implements
+//functions in interface must be created in child
+interface Animal{
+    public function makeSound();
+    public function eat();
+}
+
+class Dog implements Animal{
+    public function makeSound()
+    {
+        return "woof woof";
+    }
+
+    public function eat()
+    {
+        return "eating meat";
+    }
+}
+
+class Cat implements Animal{
+    public function makeSound()
+    {
+        return "Meow meow";
+    }
+
+    public function eat()
+    {
+        return "eating cat food";
+    }
+}
